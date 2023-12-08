94d94b5904d607effccbdae158e4367c7de3b119
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-12-08 14:16:56 +0300

    image downloader

--- image_downloader.py
+++ image_downloader.py
@@ -1,13 +1,16 @@
 import csv
 import os
 import requests
+from tqdm import tqdm
 
 
 def image_downloader(csv_file_path, month, month_number, current_year, folder_path):
     os.makedirs(f"{folder_path}/{current_year}_{month_number}", exist_ok=True)
     with open(csv_file_path, 'r') as input_file:
         reader = csv.reader(input_file)
-        for row in reader:
+
+        for row in tqdm(reader, colour='blue', ncols=1000, desc="Image downloading"):
+
             if month in row[0]:
                 image_link = row[2]
                 try:
@@ -15,6 +18,7 @@ def image_downloader(csv_file_path, month, month_number, current_year, folder_pa
                     with open(
                             f"{folder_path}/{current_year}_{month_number}/{row[0]}__{row[1]}.JPG",
                             "bw") as f:
+
                         for chunk in r.iter_content(9000):
                             f.write(chunk)
                 except Exception as ex:
@@ -24,6 +28,3 @@ def image_downloader(csv_file_path, month, month_number, current_year, folder_pa
 if __name__ == '__main__':
     image_downloader('/Volumes/big4photo/Documents/NewProspect/NP.csv',
                      'июля', 7, 2023, '/Volumes/big4photo/Documents/NewProspect')
-
-
-# https://newprospect.ru/upload/resize_cache/iblock/953/758_493_2/5iq84jvywtyyn7a3vehkopbmttwdd3ml.JPG
