f388fdff5ab1531d8d23101674c105a8530dd053
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-12-08 11:29:58 +0300

    main modul

---
+++ _main_modul.py
@@ -0,0 +1,14 @@
+from parse_np_and_download_images import get_all_images
+from select_month import select_month
+
+
+def main():
+    # select month
+    month_number = select_month()
+
+    # download all images
+    get_all_images(month_number)
+
+
+if __name__ == '__main__':
+    main()
