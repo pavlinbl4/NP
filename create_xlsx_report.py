2d3786d6f6746f58207ac4381b184cea3aaf5d4d
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-02-04 23:12:40 +0300

    refactoring

--- create_xlsx_report.py
+++ create_xlsx_report.py
@@ -27,28 +27,30 @@ worksheet = workbook.active
 worksheet.title = "Sheet with image"  # задаю название вкладки
 
 # in list widths of all columns
-worksheet = set_column_dimensions(worksheet, [25, 240, 40, 25])
+worksheet = set_column_dimensions(worksheet, [30, 600, 40, 30])
 
 thin_border = Border(left=Side(border_style="thin"),
                      right=Side(border_style="thin"),
                      top=Side(border_style="thin"),
                      bottom=Side(border_style="thin"))
+# worksheet.row_dimensions.defaultRowHeight = 130
 
 for row, image_path in enumerate(way_to_files.glob("*.JPG"), 1):
-    worksheet.row_dimensions[row].height = 150  # задаю высоту столбца
+    worksheet.row_dimensions[row].height = 130  # задаю высоту столбца
+
     print(image_path)
     img = image_resize(image_path)
 
     for column in range(1, 5):
         # cells view
         worksheet[f'{get_column_letter(column)}{row}'].border = thin_border
-        worksheet[f'{get_column_letter(column)}{row}'].font = Font(size=14, bold=True)
+        worksheet[f'{get_column_letter(column)}{row}'].font = Font(size=20, bold=True)
         worksheet[f'{get_column_letter(column)}{row}'].alignment = Alignment(horizontal='center',
                                                                              vertical='center',
                                                                              wrap_text=True)
         worksheet[f'{get_column_letter(3)}{row}'].alignment = Alignment(horizontal='center',
-                                                                             vertical='center',
-                                                                             )
+                                                                        vertical='center',
+                                                                        )
         # add information to cells
 
         worksheet[f'{get_column_letter(1)}{row}'].value = image_path.name.split("__")[0]
@@ -56,14 +58,14 @@ for row, image_path in enumerate(way_to_files.glob("*.JPG"), 1):
         worksheet.add_image(img, f"{get_column_letter(3)}{row}")
         worksheet[f'{get_column_letter(4)}{row}'].value = 500
 
-worksheet[f'{get_column_letter(4)}{row + 3}'].alignment = Alignment(horizontal='center', vertical='center')
-worksheet[f'{get_column_letter(4)}{row + 3}'].font = Font(size=17, bold=True, color="FF0000")
-worksheet[f'{get_column_letter(4)}{row + 3}'].border = thin_border
-worksheet[f'{get_column_letter(4)}{row + 3}'].value = f"=SUM(D$1:D${row})"
+for column in range(1, 5):
+    worksheet[f'{get_column_letter(column)}{row + 3}'].alignment = Alignment(horizontal='center', vertical='center')
+    worksheet[f'{get_column_letter(column)}{row + 3}'].font = Font(size=32, bold=True, color="FF0000")
+    worksheet[f'{get_column_letter(column)}{row + 3}'].border = thin_border
 
-worksheet[f'{get_column_letter(2)}{row + 3}'].font = Font(size=17, bold=True, color="FF0000")
-worksheet[f'{get_column_letter(2)}{row + 3}'].alignment = Alignment(horizontal='center', vertical='center')
-worksheet[f'{get_column_letter(2)}{row + 3}'].border = thin_border
-worksheet[f'{get_column_letter(2)}{row + 3}'].value = "ИТОГО"
+
+    worksheet.row_dimensions[row + 3].height = 60
+    worksheet[f'{get_column_letter(2)}{row + 3}'].value = "ИТОГО"
+    worksheet[f'{get_column_letter(4)}{row + 3}'].value = f"=SUM(D$1:D${row})"
 
 workbook.save(f"{way_to_files}/report_{current_year}_{month_name}.xlsx")
