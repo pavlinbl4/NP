a0ccb064a9ddd1a769850d9510ca71ad1102974c
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-02-06 19:37:22 +0300

    refactoring

--- glob_tutorial.py
+++ glob_tutorial.py
@@ -1,5 +1,5 @@
 import glob
-from icecream import ic
+from pathlib import Path
 
 
 def any_case_extension(extension: str):
@@ -11,17 +11,15 @@ def find_files_by_extension(path_to_folder: str, extension: str):
     return glob.glob(f'{path_to_folder}/*.{any_case_extension(extension)}')
 
 
-def find_files_by_extension_recursiv(path_to_folder: str, extension: str, ):
+def find_files_by_extension_recursive(path_to_folder: str, extension: str, ):
     # It looks like your glob pattern is not actually specifying to search recursively.
     # To search subfolders recursively with glob, you need to use the ** notation:
     return glob.glob(f'{path_to_folder}/**/*.{any_case_extension(extension)}', recursive=True)
 
 
-files = glob.glob('/Users/evgeniy/Downloads/*.[jJ][pP][gG]') + glob.glob('/Users/evgeniy/Downloads/*.[jJ][pP][eE][gG]')
-# print(files)
-
+files = glob.glob(f'{Path().home}/Downloads/*.[jJ][pP][gG]') + glob.glob('/Users/evgeniy/Downloads/*.[jJ][pP][eE][gG]')
+print(files)
 
 if __name__ == '__main__':
     assert (any_case_extension('jpeg')) == '[jJ][pP][eE][gG]'
     assert (any_case_extension('JPEG')) == '[jJ][pP][eE][gG]'
-
