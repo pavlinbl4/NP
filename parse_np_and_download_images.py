c18d1fd5934458078452b3bb22688c3dfd0adc99
evgeniy <pavlinbl4@gmail.com>
2024-04-01 12:03:53 +0300

    refactoring

--- parse_np_and_download_images.py
+++ parse_np_and_download_images.py
@@ -4,25 +4,23 @@ import requests
 from datetime import datetime
 from pathlib import Path
 from image_downloader import image_downloader
-from tqdm import tqdm, trange, tqdm_gui
-import os
-import datetime
-import locale
-from datetime import datetime
 
 
-def make_documets_folder(name):
-    (Path.home() / "Documents" / f"{name}").mkdir(parents=True, exist_ok=True)
-    return Path.home() / "Documents" / f"{name}"
+def make_documents_folder(name):
+    folder_path = Path.home() / "Documents" / f"{name}"
+    folder_path.mkdir(parents=True, exist_ok=True)
+    return folder_path
 
 
 def get_html(url):
     req = requests.get(url)
+    req.raise_for_status()  # Raise an exception for non-2xx status codes
     return req.text
 
 
+
 def write_csv(data, csv_file_path):
-    with open(csv_file_path, 'a') as csv_file:
+    with open(csv_file_path, 'a', newline='') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow((data["date"], data["name"], data["url"]))
 
@@ -30,10 +28,10 @@ def write_csv(data, csv_file_path):
 def get_data(html, csv_file_path):
     soup = BeautifulSoup(html, 'lxml')
     articles = soup.find_all('div', class_="l-page__main-news--item")
-    for title in articles:
-        article_date = title.find(class_="l-page__main-news--date").text.split()[-3:]  # дата публикации
-        article_name = title.find('div', class_="l-page__main-news--title").text  # заголовки статей
-        image_src = title.find('img').get('src')
+    for article in articles:
+        article_date = article.find(class_="l-page__main-news--date").text.split()[-3:]  # дата публикации
+        article_name = article.find('div', class_="l-page__main-news--title").text  # заголовки статей
+        image_src = article.find('img').get('src')
         image_link = f'https://newprospect.ru{image_src}'  # ссылка на изображение
         article_date_lookfine = " ".join(article_date)
         data = {"date": article_date_lookfine,
@@ -41,57 +39,33 @@ def get_data(html, csv_file_path):
                 "url": image_link}
         write_csv(data, csv_file_path)
 
-def month_information():
-    now = datetime.now()
 
-    # Set locale to Russian
-    locale.setlocale(locale.LC_TIME, 'ru_RU')
-
-    # Get month name in Russian
-    month_name = now.strftime("%B")
-    print(month_name)
+def date_convert(month_number):
+    months = {
+        1: 'января', 2: 'февраля', 3: 'марта', 4: 'апреля', 5: 'мая', 6: 'июня',
+        7: 'июля', 8: 'августа', 9: 'сентября', 10: 'октября', 11: 'ноября', 12: 'декабря'
+    }
+    return months.get(month_number, 'Invalid month')
 
 
-def date_convert(month_number):  # функция преобразует введенный номер месяца в его название
-    voc = {1: 'января',
-           2: 'февраля',
-           3: 'марта',
-           4: 'апреля',
-           5: 'мая',
-           6: 'июня',
-           7: 'июля',
-           8: 'августа',
-           9: 'сентября',
-           10: 'октября',
-           11: 'ноября',
-           12: 'декабря'}
-    month_name = voc[month_number]
-    return month_name
-
-
-def get_all_images(month_number):
-    url = "https://newprospect.ru"
-    current_year = datetime.now().year
-
-    folder_path = make_documets_folder('NewProspect')
-
+def get_all_images(month_number=datetime.now().month):
+    folder_path = make_documents_folder('NewProspect')
+    csv_file_path = folder_path / "NP.csv"
     month = date_convert(month_number)
-    csv_file_path = f"{folder_path}/NP.csv"
-    for i in trange(5,colour='blue', ncols = 100, desc='Main cycle'):
-        get_data(get_html(url), csv_file_path)
+
+    current_year = datetime.now().year
+    url = "https://newprospect.ru"
+    for i in range(5):
+        get_data(get_html(url),csv_file_path)
         soup = BeautifulSoup(get_html(url), 'lxml')
         url = "https://newprospect.ru" + soup.find('li', class_='l-nav__pagination-item next').find('a').get('href')
-    # print('CSV file created')
+    print('CSV file created')
     image_downloader(csv_file_path, month, month_number, current_year, folder_path)
     print('Images downloaded - please select you images and run "create_report" script')
-    os.remove(csv_file_path)
-    return folder_path
 
 
 if __name__ == '__main__':
-    # get_all_images(3)
-    month_information()
 
 
 
-
+    get_all_images(4)
