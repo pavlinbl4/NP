39c931f3b3ca8ffa1643bfa799e126c1e8c52ab6
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-08-04 22:06:04 +0300

    refactoring

--- all_abstract/get_data_from_page.py
+++ all_abstract/get_data_from_page.py
@@ -3,26 +3,30 @@ from bs4 import BeautifulSoup
 from loguru import logger
 
 main_url = 'https://newprospect.ru'
-
-count = None
-work_url_path = f'?article={count}&pageId=1&hash=cbf05cb35393866fb263316a822d653e4971ece5'
-
 articles_on_page_selector = "tplarticle"
 
 
 def work_with_selected_page(count):
-    work_url: str = f"{main_url}/{work_url_path}"
-    logger.info(work_url)
-    html = requests.get(work_url).text
-    soup = BeautifulSoup(html, 'lxml')
+    work_url_path = f'?article={count}&pageId=1&hash=cbf05cb35393866fb263316a822d653e4971ece5'
+    work_url = f"{main_url}/{work_url_path}"
+    logger.info(f"Requesting URL: {work_url}")
 
-    logger.info(articles_on_page_selector)
-    articles = soup.find_all("div", class_=articles_on_page_selector)
-    logger.info(len(articles))
+    response = requests.get(work_url)
+    if response.status_code == 200:
+        html = response.text
+        soup = BeautifulSoup(html, 'lxml')
 
-    return articles
+        articles = soup.find_all("div", class_=articles_on_page_selector)
+        logger.info(f"Found {len(articles)} articles on the page.")
+
+        return articles
+    else:
+        logger.error(f"Failed to retrieve the page. Status code: {response.status_code}")
+        return []
 
 
 if __name__ == '__main__':
-    print(work_with_selected_page(0))
-
+    count = 0
+    articles = work_with_selected_page(count)
+    for article in articles:
+        print(article)
