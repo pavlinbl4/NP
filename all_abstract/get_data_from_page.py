aa9b0eae0a9f83ad57df2e19b1f5d1dcc239e59b
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-08-04 21:42:27 +0300

    send selector only to class_

--- all_abstract/get_data_from_page.py
+++ all_abstract/get_data_from_page.py
@@ -7,7 +7,7 @@ main_url = 'https://newprospect.ru'
 count = None
 work_url_path = f'?article={count}&pageId=1&hash=cbf05cb35393866fb263316a822d653e4971ece5'
 
-articles_on_page_selector = ("div", 'class_="row gy-3 gy-lg-4"')
+articles_on_page_selector = "tplarticle"
 
 
 def work_with_selected_page(count):
@@ -17,10 +17,12 @@ def work_with_selected_page(count):
     soup = BeautifulSoup(html, 'lxml')
 
     logger.info(articles_on_page_selector)
-    articles = soup.find_all(articles_on_page_selector)
+    articles = soup.find_all("div", class_=articles_on_page_selector)
+    logger.info(len(articles))
 
     return articles
 
 
 if __name__ == '__main__':
-    print(work_with_selected_page(1))
+    print(work_with_selected_page(0))
+
